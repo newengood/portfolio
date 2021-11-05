4667ce1d44f3219f833898ca1532c683a5e3f1af
newengood <04-scones.card@newengood.com>
2021-11-05 00:17:01 -0700

    doc update

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,10 +1,11 @@
-import Resume_ from '../docs/Taylor-R-Anderson.pdf'
+import 
+myResume from '../docs/Taylor-R-Anderson.pdf'
 
 // Render resume
 const Resume = () => {
   return (
     <div>
-      <iframe src={Resume_} title="resume" width="100%" height="750px"></iframe>
+      <iframe src={MyResume} title="resume" width="100%" height="750px"></iframe>
     </div>
   );
 };
