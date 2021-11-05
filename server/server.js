9da707ff990a38e62bf73cd58080f92ef0e2a1c0
newengood <04-scones.card@newengood.com>
2021-11-04 17:52:48 -0700

    serve build in production

--- server/server.js
+++ server/server.js
@@ -9,6 +9,12 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// If we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+
 // Home route
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
